d5364a7b8ee79f41db99ea99304cfecad313d70b
0xrm1 <0xrm1@users.noreply.github.com>
2025-03-15 01:06:39 +0300

    Fix: Update API URLs to use HTTP protocol instead of HTTPS

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://quicky-trade-env.eba-my3ep4mp.us-east-1.elasticbeanstalk.com/api/:path*'
+        destination: 'http://quicky-trade-env.eba-my3ep4mp.us-east-1.elasticbeanstalk.com/api/:path*'
       }
     ]
   },
@@ -15,7 +15,7 @@ const nextConfig = {
         source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://quicky-trade-env.eba-my3ep4mp.us-east-1.elasticbeanstalk.com' },
+          { key: 'Access-Control-Allow-Origin', value: 'http://quicky-trade-env.eba-my3ep4mp.us-east-1.elasticbeanstalk.com' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
           { key: 'Access-Control-Max-Age', value: '86400' }
